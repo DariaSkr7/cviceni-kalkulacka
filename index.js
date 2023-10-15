faef01a5e05c4c5fa03544b7890cc0a825f7f0b4
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-15 16:20:16 +0200

    solution

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const handleDigitClick = (event) => {
+  const cifra = event.target.textContent;
+  const display = document.querySelector('.display');
+  if (display.textContent === '0') {
+    display.textContent = '';
+  }
+  if (display.textContent.length <= 9) {
+    display.textContent += cifra;
+  }
+};
 
+const buttons = document.querySelectorAll('.btn-num');
+buttons.forEach((btn) => {
+  btn.addEventListener('click', handleDigitClick);
+});
